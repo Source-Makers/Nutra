98e6488af10255af693a46cd4c0a3658313eafa8
theSirD <isaev-daniel17@yandex.ru>
2023-08-02 18:46:40 +0300

    Create layout for right sidebar

--- Nutra/ClientApp/src/components/layout/savedRecipes/SavedRecipes.jsx
+++ Nutra/ClientApp/src/components/layout/savedRecipes/SavedRecipes.jsx
@@ -1,3 +1,27 @@
+import Suggestion from "../chatArea/messageArea/listOfSuggestions/Suggestion";
 export default function SavedRecipes() {
-    return <div className="saved-recipes-container"></div>
+  return (
+    <div className="saved-recipes-container">
+      <div className="saved-recipes-header-container">
+        <div className="saved-recipes-header">Saved recipes</div>
+      </div>
+
+      <div className="list-of-saved-recipes">
+        <Suggestion title="Fried eggs and chicken" />
+        <Suggestion title="Fried eggs and chicken" />
+        <Suggestion title="Fried eggs and chicken" />
+        <Suggestion title="Fried eggs and chicken" />
+        <Suggestion title="Fried eggs and chicken" />
+        <Suggestion title="Fried eggs and chicken" />
+        <Suggestion title="Fried eggs and chicken" />
+        <Suggestion title="Fried eggs and chicken" />
+        <Suggestion title="Fried eggs and chicken" />
+      </div>
+      <div className="saved-recipes-button-container">
+        <button type="button" className="delete-all-recipes-button">
+          Delete all recipes
+        </button>
+      </div>
+    </div>
+  );
 }
