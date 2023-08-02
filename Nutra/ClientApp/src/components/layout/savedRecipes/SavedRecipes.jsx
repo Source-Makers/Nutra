46a98e60b2cc5cd970fd6a5cb23f5ff3851f5a1b
theSirD <isaev-daniel17@yandex.ru>
2023-08-02 19:03:27 +0300

    Enhances layout of sidebar

--- Nutra/ClientApp/src/components/layout/savedRecipes/SavedRecipes.jsx
+++ Nutra/ClientApp/src/components/layout/savedRecipes/SavedRecipes.jsx
@@ -2,26 +2,24 @@ import Suggestion from "../chatArea/messageArea/listOfSuggestions/Suggestion";
 export default function SavedRecipes() {
   return (
     <div className="saved-recipes-container">
-      <div className="saved-recipes-header-container">
+      <div className="extra-container">
         <div className="saved-recipes-header">Saved recipes</div>
+        <div className="list-of-saved-recipes">
+          <Suggestion title="Fried eggs and chicken" />
+          <Suggestion title="Fried eggs and chicken" />
+          <Suggestion title="Fried eggs and chicken" />
+        <Suggestion title="Fried eggs and chicken" />
+        <Suggestion title="Fried eggs and chicken" />
+        <Suggestion title="Fried eggs and chicken" />
+        <Suggestion title="Fried eggs and chicken" />
+        <Suggestion title="Fried eggs and chicken" />
+        <Suggestion title="Fried eggs and chicken" />
+        </div>
       </div>
 
-      <div className="list-of-saved-recipes">
-        <Suggestion title="Fried eggs and chicken" />
-        <Suggestion title="Fried eggs and chicken" />
-        <Suggestion title="Fried eggs and chicken" />
-        <Suggestion title="Fried eggs and chicken" />
-        <Suggestion title="Fried eggs and chicken" />
-        <Suggestion title="Fried eggs and chicken" />
-        <Suggestion title="Fried eggs and chicken" />
-        <Suggestion title="Fried eggs and chicken" />
-        <Suggestion title="Fried eggs and chicken" />
-      </div>
-      <div className="saved-recipes-button-container">
-        <button type="button" className="delete-all-recipes-button">
-          Delete all recipes
-        </button>
-      </div>
+      <button type="button" className="delete-all-recipes-button">
+        Delete all recipes
+      </button>
     </div>
   );
 }
