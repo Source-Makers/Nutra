4d16638c2bbe9b1d5aa6fb00a811aeb6f98f2987
theSirD <isaev-daniel17@yandex.ru>
2023-08-03 15:24:03 +0300

    Improves message-area even more

--- Nutra/ClientApp/src/components/layout/chatArea/messageArea/messageArea.jsx
+++ Nutra/ClientApp/src/components/layout/chatArea/messageArea/messageArea.jsx
@@ -6,12 +6,10 @@ export default function MessageArea() {
   return (
     <div className="message-area">
       <ListOfSuggestions sender="bot" />
+      <ListOfSuggestions sender="bot" />
+      <ListOfSuggestions sender="bot" />
       <ListOfFindings sender="bot" />
-      <MessageWithPhoto path="./photoesFromUser/1.jpg" sender="bot" />
-
       <MessageWithPhoto path="./photoesFromUser/1.jpg" sender="user" />
-      <ListOfSuggestions sender="user" />
-      <ListOfFindings sender="user" />
     </div>
   );
 }
